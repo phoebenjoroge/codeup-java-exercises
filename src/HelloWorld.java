3e36064d1592004637a54ab9045b8d02c962e6b7
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-21 13:34:23 -0600

    practicing java syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //SYNTAX EXERCISE
+        //Number One
+        int myFavoriteNumber =12;
+        System.out.println(myFavoriteNumber);
+        //Number TWO
+        String myString = "Hello, Phoebe";
+        //Number Three
+        myString ='C';
     }
 }
