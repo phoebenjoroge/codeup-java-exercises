9a2dd785e719dfb2c33c0a309a2747e1e205a857
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-21 13:43:26 -0600

    completed number 6-8 of syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,6 +17,14 @@ public class HelloWorld {
         */
         //Number 5 - Gives error and says variable might not have been initialized
         long myNumber;
-        System.out.println(myNumber);
+        //System.out.println(myNumber);
+        /*Number 6 -Gives an error says requires long not double
+        myNumber = 3.14;
+        */
+        //Number 7
+        myNumber =123L;
+        //Number 8 -long does not take in decimal numbers however does take in whole numbers
+        myNumber =123;
+
     }
 }
