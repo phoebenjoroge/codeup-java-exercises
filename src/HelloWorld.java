80caa997609f7906c3cb3d2001473c3407071894
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-21 14:14:02 -0600

    completed syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -25,6 +25,49 @@ public class HelloWorld {
         myNumber =123L;
         //Number 8 -long does not take in decimal numbers however does take in whole numbers
         myNumber =123;
+        //Number 9 -says number is already defined in the scope. To reassign, myNumber needs to be casted to a float
+        //float myNumber = 3.14;
+        float floatingNumber =myNumber; //implicit casting
+        float pi = (float) myNumber;  //Explicit casting
+        //Number 10
+        int x = 5;
+        System.out.println("Print x++ " + x++); //Code block one post increments therefore first line prints x as is then increments x
+        System.out.println("Print x " + x); //and new value of x is incremented
+
+         x = 5;
+        System.out.println("Print ++x " + ++x); //Code block two increments x then prints
+        System.out.println("Print x " + x);     //therefore x remains the same as it was incremented in the previous line prior to printing
+
+        //Number 11
+        //String class = "Class"; cannot create a class variable as 'class' is a reserved word
+
+        //Number 12
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        System.out.println(o);
+        //int three = (int) o; gives an error for casting String into an integer
+
+        //Number 13
+        x= 4;
+        x+=5;
+        System.out.println(x);
+
+        x=3;
+        int y=4;
+        y *=x;
+        System.out.println(y);
+
+        x= 10;
+        y=2;
+        x/=y;
+        System.out.println(x);
+        y-=x;
+        System.out.println(y);
+
+        //Number 14 - assigning a larger numerical value than the type can hold will return the smallest value the type can hold and vice versa for a smaller assignment
+        //incrementing past the type's capacity will also cause the type to loop and start from the smallest value the type can hold
+        byte number = -128;
+        System.out.println(--number);
 
     }
 }
