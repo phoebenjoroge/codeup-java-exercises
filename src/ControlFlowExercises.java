17a8f842ec4675bb1ad7e5fdaa57d05ea10cdf60
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-23 14:20:56 -0600

    begining control flow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ControlFlowExercises {
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i= 5;
+
+        //Question 1: WHILE Loop
+        while(i<=15){
+            System.out.println("Value of i is: " +i);
+            i++;
+        }
+
+        //Question 2: DO WHILE loop
+
+    }
 }
