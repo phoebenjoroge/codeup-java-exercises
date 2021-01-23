301247366ee0c38ae688bffc7e79f40dd5a1d31a
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-23 14:51:41 -0600

    completed question 2 for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -21,10 +21,22 @@ public class ControlFlowExercises {
 //            y-=5;
 //        }while(y>=-10);
         //do while-3
-        double z=2;
-        do{
-            System.out.println(z);
-            z = Math.pow(z, 2);
-        }while(z<=1000000);
+//        double z=2;
+//        do{
+//            System.out.println(z);
+//            z = Math.pow(z, 2);
+//        }while(z<=1000000);
+//
+//        // Question 1C
+//        for(int a =2; a<=100; a+=2){
+//            System.out.println(a);
+//        }
+//        for(int b =100; b>=-10; b-=5){
+//            System.out.println(b);
+//        }
+//        for(double c=2; c<=1000000; c*=c){
+//            System.out.println(c);
+//        }
+
     }
 }
