d5906239892e53b466e1a786ab58e57d1ddedc80
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-25 13:35:29 -0600

    updating Bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Communicate with Bob : ");
+        String bobCommunication = input.nextLine();
+        if(bobCommunication.endsWith("?")){
+            System.out.println("Sure");
+        }else if(bobCommunication.endsWith("!")){
+            System.out.println("Whoa, chill out!");
+        }else if(bobCommunication.equals("")){
+            System.out.println("Fine. Be that way!");
+        }else{
+            System.out.println("Whatever");
+        }
+    }
+}
