e40a39a1a1d2ae9bd96bfa408a301628eb13bf10
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-26 12:20:49 -0600

    completed number one on method exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,26 @@
 public class MethodsExercises {
     public static void main(String[] args) {
-
+        long add = addition(3,5);
+        System.out.println(add);
     }
+    //Number 1 -Functions that perform basic arithmetic
+    public static long addition(int num1, int num2) {
+        return num1+ num2;
+    }
+    public static long subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+    public static long multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+    public static long division(int num1, int num2) {
+        return num1 / num2;
+    }
+    public static long modulus(int num1, int num2) {
+        return num1 % num2;
+    }
+    public static long multiplicationReloaded(int num1, int num2) {
 
+        return num2;
+    }
 }
