e639bc511258c0d24eb19bdb48fd22855b9d0d80
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-26 19:24:17 -0600

    completed high low exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -3,7 +3,26 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String[] args) {
+        randomNumber();
+
+    }
+
+    public static String randomNumber(){
         Random rand = new Random();
         Scanner input =new Scanner(System.in);
+        System.out.print("Pick a number between 1 and 100 : ");
+        int userNumber = input.nextInt();
+        int randomNumber = rand.nextInt(100);
+        System.out.println(randomNumber);
+            if(userNumber == randomNumber){
+                System.out.println("GOOD  GUESS");
+                return "Game over, you won!";
+            }else if(userNumber > randomNumber){
+                System.out.println("LOWER");
+            }else if(userNumber < randomNumber){
+                System.out.println("HIGHER");
+            }
+            return randomNumber();
+
     }
 }
