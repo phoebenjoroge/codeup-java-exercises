71c038a11a50c52ac7b1113494c9652fc1be7b04
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-26 19:01:20 -0600

    begining high low exercise

---
+++ src/HighLow.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HighLow {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner input =new Scanner(System.in);
+    }
+}
