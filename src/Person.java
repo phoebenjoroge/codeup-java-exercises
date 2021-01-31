901933602bf28cfdb5c44b56f7395a0c164b0e76
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-31 16:51:12 -0600

    updating OOP exercise

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private String name;
+
+    public String getName() {
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name) {
+//TODO: change the name property to the passed value
+       Person phoebe = new Person();
+       phoebe.name = "Phoebe";
+        System.out.println();
+    }
+
+    public void sayHello() {
+//TODO: print a message to the console using the person's name
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
