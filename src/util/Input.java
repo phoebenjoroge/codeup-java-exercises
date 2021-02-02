127ed8fea7cfc84aa8ff095adc312cdd06be50b1
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-02-02 11:18:36 -0600

    updating util OOP exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,46 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    private Scanner scanner =new Scanner(System.in);
+
+    Input(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    String getString(){
+        return this.scanner.nextLine();
+    }
+    boolean yesNo(){
+        return this.scanner.nextBoolean();
+    }
+    int getInt(){
+        return this.scanner.nextInt();
+    }
+    int getInt(int min, int max){
+        while(true){
+            int userInput = this.scanner.nextInt();
+            if(userInput >= min && userInput <= max){
+                return userInput;
+            }
+        }
+    }
+    double getDouble(){
+        return this.scanner.nextDouble();
+    }
+    double getDouble(double min, double max){
+        while(true){
+            double userInput = this.scanner.nextDouble();
+            if(userInput >= min && userInput <= max){
+                return userInput;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
 }
