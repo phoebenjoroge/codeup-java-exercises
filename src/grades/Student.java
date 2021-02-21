5505a1a5c1b3951067975015ced8271168d591c5
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-02-20 21:42:31 -0600

    updating grades exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,2 +1,24 @@
-package grades;public class Student {
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String studentName;
+//    private char grades;
+    ArrayList<Double> grades = new ArrayList<>();
+
+    public String getName(){
+        return this.studentName;
+    };
+    public void addGrade(int grade){
+
+    };
+    public double getGradeAverage(){
+        double average = 0;
+        return average;
+    };
+
+    public static void main(String[] args) {
+
+    }
 }
